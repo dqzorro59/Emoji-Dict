f3a62d9d943bd6ad9fb6b53d2f95fe15fc319b15
Dennis Quinones2 <dquinones2@cfl.rr.com>
2017-01-03 09:11:54 -0500

    Added emoji list

--- Emoji Dict/ViewController.swift
+++ Emoji Dict/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var emojiTableView: UITableView!
+    
+    var emojiList = ["😎","💩","🤑","🐹","😈"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiTableView.dataSource = self
+        emojiTableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return emojiList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = emojiList[indexPath.row]
+        
+        return cell
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
