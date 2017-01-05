25a43dce78b354944642835d8a52a37009a6b6bf
Dennis Quinones2 <dquinones2@cfl.rr.com>
2017-01-05 17:55:49 -0500

    finish

---
+++ Emoji Dict/CEmoji.swift
@@ -0,0 +1,16 @@
+//
+//  CEmoji.swift
+//  Emoji Dict
+//
+//  Created by Dennis Quinones2 on 1/4/17.
+//  Copyright © 2017 Dennis Quinones. All rights reserved.
+//
+
+class CEmoji{
+    var symbol = ""
+    var definition = ""
+    var category = ""
+    var birthYear = 0
+    
+    
+}
--- Emoji Dict/ViewController.swift
+++ Emoji Dict/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var emojiTableView: UITableView!
     
 
-    var emojiList = ["😎","💩","🤑","🐹","😈"]
+    var emojiList : [CEmoji] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +21,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         emojiTableView.dataSource = self
         emojiTableView.delegate = self
+        emojiList = createEmojiList()
+    
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,8 +33,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell()
+        let emoji = emojiList[indexPath.row]
         
-        cell.textLabel?.text = emojiList[indexPath.row]
+        cell.textLabel?.text = emoji.symbol
+        
         
         return cell
         
@@ -50,9 +54,43 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let defVC = segue.destination as! DefinitionViewController
+        defVC.emoji = sender as! CEmoji
         
+    }
+    
+    func createEmojiList() -> [CEmoji] {
         
-        defVC.emoji = sender as! String
+        let emoji1 = CEmoji()
+        emoji1.symbol = "😎"
+        emoji1.definition = "Movie Star"
+        emoji1.category = "Faces"
+        emoji1.birthYear = 2009
+        
+        let emoji2 = CEmoji()
+        emoji2.symbol = "💩"
+        emoji2.definition = "Poo Face"
+        emoji2.category = "Faces"
+        emoji2.birthYear = 2015
+        
+        let emoji3 = CEmoji()
+        emoji3.symbol = "🤑"
+        emoji3.definition = "Silly Face"
+        emoji3.category = "Faces"
+        emoji3.birthYear = 2011
+        
+        let emoji4 = CEmoji()
+        emoji4.symbol = "😈"
+        emoji4.definition = "Evil Face with Horns"
+        emoji4.category = "Faces"
+        emoji4.birthYear = 2014
+        
+        let emoji5 = CEmoji()
+        emoji5.symbol = "🐹"
+        emoji5.definition = "Cute Hamster"
+        emoji5.category = "Animal"
+        emoji5.birthYear = 2015
+        
+        return [emoji1, emoji2, emoji3, emoji4, emoji5 ]
         
     }
     
